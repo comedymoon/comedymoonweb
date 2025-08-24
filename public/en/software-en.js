2e40eeeaf2c548936da8aea327e4ee9a3273d32b
comedymoon <kdv.usb@gmail.com>
2025-08-24 17:50:08 +0300

    Add files via upload

---
+++ public/en/software-en.js
@@ -0,0 +1,37 @@
+
+const t = { download: "Download", choose: "Choose a download source", close: "Close" };
+const programs = {
+  vncreaper1: {
+    title: "VNCReaper 1.0, 1.2 (10 Aug 2025)",
+    short: "Next generation of VNCReaper",
+    long: "Next-gen VNCReaper: VNC/noVNC scanner with possible brute-force support. Lots of features; at 2k–3k threads ~300–500 scans/s, then drops to ~50.",
+    links: {
+      github:"https://github.com/comedymoon/VNCReaper",
+      zip:"https://comedymoon.lol/files/vncreaper-v1.2-windows-amd64.zip",
+      gdrive:"https://drive.google.com/file/d/1tpWR_GDne2lTN8iVMRuspjqdw_OCiNCa/view?usp=sharing",
+      mega:"https://mega.nz/file/LLphCDqS#vRmLntldOC_3aXRLLMoR48PDoZUt2hMS6vV7EwUiOXA"
+    }
+  },
+  vncreaper05: {
+    title: "VNCReaper 0.5 (05 Aug 2025)",
+    short: "Previous generation of VNCReaper",
+    long: "A very fast VNC (RFB) scanner without brute-forcing.",
+    links: {
+      github:"https://github.com/comedymoon/VNCReaper05/",
+      zip:"https://comedymoon.lol/files/vncreaper-v0.5-windows-amd64.zip",
+      gdrive:"https://drive.google.com/file/d/1fA2qHxWzdj4Nz-c9RO8NtzKWJFLmHRbm/view?usp=sharing",
+      mega:"https://mega.nz/file/iGgWgbiQ#8bR6fSokfUhdkmivqjP8YHHv5Co69w5Y9GRF8HKFZ30"
+    }
+  },
+  yagey123: {
+    title: "yagey123 (23 Aug 2025)",
+    short: "Prank app",
+    long: "A joke “virus” that spams the screen with Arabic text and memes.",
+    links: {
+      github:"https://github.com/comedymoon/yagey123",
+      zip:"https://comedymoon.lol/files/yagey123.exe",
+      gdrive:"https://drive.google.com/file/d/1IMMw975E-Ily8XOI8qARjFq88s0buRwa/view?usp=sharing",
+      mega:"https://mega.nz/file/SHwVkDTY#CcJ7v1JsL2QSO4nU2eywfPdGpVOd-UDQV_gpPK_oG4E"
+    }
+  }
+};
