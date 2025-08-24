04c709843e8f95b6b1b977c2b361f4e57d19eb40
comedymoon <kdv.usb@gmail.com>
2025-08-24 17:51:12 +0300

    Add files via upload

---
+++ public/ua/software-ua.js
@@ -0,0 +1,37 @@
+
+const t = { download: "Завантажити", choose: "Виберіть джерело завантаження", close: "Закрити" };
+const programs = {
+  vncreaper1: {
+    title: "VNCReaper 1.0, 1.2 (10.08.25)",
+    short: "Нове покоління VNCReaper",
+    long: "Нове покоління VNCReaper: сканер/можливий брутфорс VNC/noVNC. Багато фіч; на 2000–3000 тредів ~300–500 сканів/с, потім падає до ~50.",
+    links: {
+      github:"https://github.com/comedymoon/VNCReaper",
+      zip:"https://comedymoon.lol/files/vncreaper-v1.2-windows-amd64.zip",
+      gdrive:"https://drive.google.com/file/d/1tpWR_GDne2lTN8iVMRuspjqdw_OCiNCa/view?usp=sharing",
+      mega:"https://mega.nz/file/LLphCDqS#vRmLntldOC_3aXRLLMoR48PDoZUt2hMS6vV7EwUiOXA"
+    }
+  },
+  vncreaper05: {
+    title: "VNCReaper 0.5 (05.08.25)",
+    short: "Старе покоління VNCReaper",
+    long: "Дуже швидкий сканер VNC (RFB) без брутфорсу.",
+    links: {
+      github:"https://github.com/comedymoon/VNCReaper05/",
+      zip:"https://comedymoon.lol/files/vncreaper-v0.5-windows-amd64.zip",
+      gdrive:"https://drive.google.com/file/d/1fA2qHxWzdj4Nz-c9RO8NtzKWJFLmHRbm/view?usp=sharing",
+      mega:"https://mega.nz/file/iGgWgbiQ#8bR6fSokfUhdkmivqjP8YHHv5Co69w5Y9GRF8HKFZ30"
+    }
+  },
+  yagey123: {
+    title: "yagey123 (23.08.25)",
+    short: "Жартівлива програма",
+    long: "Жартівливий "вірус", що спамить екран арабським текстом і мемами.",
+    links: {
+      github:"https://github.com/comedymoon/yagey123",
+      zip:"https://comedymoon.lol/files/yagey123.exe",
+      gdrive:"https://drive.google.com/file/d/1IMMw975E-Ily8XOI8qARjFq88s0buRwa/view?usp=sharing",
+      mega:"https://mega.nz/file/SHwVkDTY#CcJ7v1JsL2QSO4nU2eywfPdGpVOd-UDQV_gpPK_oG4E"
+    }
+  }
+};
